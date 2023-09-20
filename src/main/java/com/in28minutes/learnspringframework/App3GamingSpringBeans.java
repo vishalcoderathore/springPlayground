19b65105b0263b1129ac1d61880eb423cf4b91d7
Vishal Rathore <vshl.rathore87@gmail.com>
2023-09-20 16:54:00 -0500

    Removed unused files, and combined the GamingConfiguration with App3GamingSpringBeans

--- src/main/java/com/in28minutes/learnspringframework/App3GamingSpringBeans.java
+++ src/main/java/com/in28minutes/learnspringframework/App3GamingSpringBeans.java
@@ -1,9 +1,30 @@
 package com.in28minutes.learnspringframework;
-
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 import com.in28minutes.learnspringframework.game.GameRunner;
 import com.in28minutes.learnspringframework.game.GameRunnerInterface;
+import com.in28minutes.learnspringframework.game.PackmanGame;
+
+@Configuration
+class GamingConfiguration {
+    // Creating a Packman bean
+    @Bean
+    public GameRunnerInterface game(){
+        var game = new PackmanGame();
+        return game;
+    }
+
+    // Wiring Packman bean to GameRunner
+    @Bean
+    public GameRunner gameRunner(GameRunnerInterface game){
+        var gameRunner = new GameRunner((game));
+        return gameRunner;
+    }
+
+    // game and gameRunner are Spring Beans
+}
 
 public class App3GamingSpringBeans {
  
