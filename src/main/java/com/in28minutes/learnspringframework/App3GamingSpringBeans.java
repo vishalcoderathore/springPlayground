26796f40b92ecd9fdd8b3194a33ca52c8a17b864
Vishal Rathore <vshl.rathore87@gmail.com>
2023-09-20 16:48:54 -0500

    Added Beans and and context

--- src/main/java/com/in28minutes/learnspringframework/App3GamingSpringBeans.java
+++ src/main/java/com/in28minutes/learnspringframework/App3GamingSpringBeans.java
@@ -1,5 +1,18 @@
 package com.in28minutes.learnspringframework;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.in28minutes.learnspringframework.game.GameRunner;
+import com.in28minutes.learnspringframework.game.GameRunnerInterface;
+
 public class App3GamingSpringBeans {
-    
+ 
+    public static void main(String[] args) {
+        try(var context = new AnnotationConfigApplicationContext(GamingConfiguration.class)){
+            
+            // Picking and running Spring beans from Spring Context
+            context.getBean(GameRunnerInterface.class).up();
+            context.getBean(GameRunner.class).run();
+        }
+    }
 }
