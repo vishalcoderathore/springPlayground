f5dec737fcbf965d315637220199a5bffdce3df9
Vishal Rathore <vshl.rathore87@gmail.com>
2023-09-20 17:31:40 -0500

    Added @Primary and @Qualifier annotations to decide which bean to use

--- src/main/java/com/in28minutes/learnspringframework/game/GameRunner.java
+++ src/main/java/com/in28minutes/learnspringframework/game/GameRunner.java
@@ -1,12 +1,13 @@
 package com.in28minutes.learnspringframework.game;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class GameRunner {
 
     private GameRunnerInterface game;
-    public GameRunner(GameRunnerInterface game){
+    public GameRunner(@Qualifier("PackmanQualifier") GameRunnerInterface game){
         this.game = game;
     }
 
--- src/main/java/com/in28minutes/learnspringframework/game/PackmanGame.java
+++ src/main/java/com/in28minutes/learnspringframework/game/PackmanGame.java
@@ -1,8 +1,10 @@
 package com.in28minutes.learnspringframework.game;
 
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
+@Qualifier("PackmanQualifier")
 public class PackmanGame implements GameRunnerInterface{
     public void up(){
         System.out.println("Packman UP");
