0ea1a4200d3d58241319df106de4b4c5dced22d2
Vishal Rathore <vshl.rathore87@gmail.com>
2023-09-20 16:57:00 -0500

    Merged the configuration class code with App3GamingSpringBeans class. Removed the configuration class and updated the App3GamingSpringBeans as the new configuration class

--- src/main/java/com/in28minutes/learnspringframework/App3GamingSpringBeans.java
+++ src/main/java/com/in28minutes/learnspringframework/App3GamingSpringBeans.java
@@ -8,8 +8,8 @@ import com.in28minutes.learnspringframework.game.GameRunnerInterface;
 import com.in28minutes.learnspringframework.game.PackmanGame;
 
 @Configuration
-class GamingConfiguration {
-    // Creating a Packman bean
+public class App3GamingSpringBeans {
+
     @Bean
     public GameRunnerInterface game(){
         var game = new PackmanGame();
@@ -22,14 +22,9 @@ class GamingConfiguration {
         var gameRunner = new GameRunner((game));
         return gameRunner;
     }
-
-    // game and gameRunner are Spring Beans
-}
-
-public class App3GamingSpringBeans {
  
     public static void main(String[] args) {
-        try(var context = new AnnotationConfigApplicationContext(GamingConfiguration.class)){
+        try(var context = new AnnotationConfigApplicationContext(App3GamingSpringBeans.class)){
             
             // Picking and running Spring beans from Spring Context
             context.getBean(GameRunnerInterface.class).up();
