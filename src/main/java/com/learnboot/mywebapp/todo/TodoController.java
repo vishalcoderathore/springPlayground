1d12e5eaecbd87a4f781cd96b0045c8ce6a18263
Vishal Rathore <vshl.rathore87@gmail.com>
2023-10-04 21:03:54 -0500

    added jstl library

---
+++ src/main/java/com/learnboot/mywebapp/todo/TodoController.java
@@ -0,0 +1,23 @@
+package com.learnboot.mywebapp.todo;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class TodoController {
+    public TodoController(TodoService todoService) {
+        this.todoService = todoService;
+    }
+
+    private TodoService todoService;
+
+    @RequestMapping("list-todos")
+    public String listAllTodos(ModelMap model) {
+        List<Todo> todos = todoService.findByUsername("vishal");
+        model.addAttribute("todos", todos);
+        return "listTodos";
+    }
+}
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/listTodos.jsp
@@ -0,0 +1,13 @@
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Java Spring Todo</title>
+  </head>
+  <body>
+    List Todos page You todos are : ${todos}
+  </body>
+</html>
